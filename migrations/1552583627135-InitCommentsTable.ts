6003f9a541690bae31f187bf647dd1ae80fc74b9
davran-sabiraliev <davransat@gmail.com>
2019-03-14 23:21:34 +0600

    Added several migrations

---
+++ migrations/1552583627135-InitCommentsTable.ts
@@ -0,0 +1,35 @@
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from 'typeorm'
+
+export class InitCommentsTable1552583627135 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.createTable(new Table({
+      name: 'comments',
+      columns: [
+        { name: 'id', type: 'UUID', default: 'uuid_generate_v4()', isPrimary: true, isUnique: true },
+        { name: 'comment_text', type: 'VARCHAR', length: '1000' },
+        { name: 'author_id', type: 'UUID' },
+        { name: 'replied_to_comment_id', type: 'UUID' },
+        { name: 'created_at', type: 'TIMESTAMP WITH TIME ZONE', default: 'NOW()' },
+        { name: 'updated_at', type: 'TIMESTAMP WITH TIME ZONE', isNullable: true }
+      ]
+    }), true)
+
+    await queryRunner.createForeignKey('comments', new TableForeignKey({
+      columnNames: ['author_id'],
+      referencedColumnNames: ['id'],
+      referencedTableName: 'users'
+    }))
+
+    await queryRunner.createForeignKey('comments', new TableForeignKey({
+      columnNames: ['replied_to_comment_id'],
+      referencedColumnNames: ['id'],
+      referencedTableName: 'comments'
+    }))
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.dropTable('comments', true)
+  }
+
+}
--- migrations/1552584029911-InitProductReviewsTable.ts
+++ migrations/1552584029911-InitProductReviewsTable.ts
@@ -1,19 +1,20 @@
 import { MigrationInterface, QueryRunner, Table, TableForeignKey } from 'typeorm'
 
-export class InitProductImagesTable1552582574068 implements MigrationInterface {
+export class InitProductFeedbackTable1552583242175 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.createTable(new Table({
-      name: 'product_images',
+      name: 'product_reviews',
       columns: [
         { name: 'id', type: 'UUID', default: 'uuid_generate_v4()', isPrimary: true, isUnique: true },
-        { name: 'url', type: 'VARCHAR' },
+        { name: 'mark', type: 'SMALLINT' },
+        { name: 'product_id', type: 'UUID' },
         { name: 'created_at', type: 'TIMESTAMP WITH TIME ZONE', default: 'NOW()' },
-        { name: 'product_id', type: 'UUID' }
+        { name: 'updated_at', type: 'TIMESTAMP WITH TIME ZONE', isNullable: true }
       ]
     }), true)
 
-    await queryRunner.createForeignKey('product_images', new TableForeignKey({
+    await queryRunner.createForeignKey('product_reviews', new TableForeignKey({
       columnNames: ['product_id'],
       referencedColumnNames: ['id'],
       referencedTableName: 'products'
@@ -21,7 +22,7 @@ export class InitProductImagesTable1552582574068 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<any> {
-    await queryRunner.dropTable('product_images', true)
+    await queryRunner.dropTable('product_reviews', true)
   }
 
 }
